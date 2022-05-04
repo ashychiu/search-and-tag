4f6a05abd9a2875cc47bb5ab4ca119442135dab5
ashychiu <ashychiu@gmail.com>
2022-05-03 23:39:55 -0700

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import React from "react";
 
-function App() {
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://api.hatchways.io/assessment/students");
+        const response = await res.json();
+        setStudents(response.students);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div key={students.number} className="container">
+      {students.map((student) => {
+        return (
+          <div>
+            <img src={student.pic} alt="profile pic" />
+            <p>
+              {student.firstName} {student.lastName}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
