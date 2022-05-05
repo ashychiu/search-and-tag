68300ca32739755acd8318ea486b97ccbb505365
ashychiu <ashychiu@gmail.com>
2022-05-04 23:28:35 -0700

    Part 2 & 3 completed. Added search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-// import "./styles/global.scss";
+import "./styles/global.scss";
 import { useEffect, useState } from "react";
 import React from "react";
 
 const App = () => {
   const [students, setStudents] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +20,26 @@ const App = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const filterStudentList = (query) => {
+      const searchTerm = query.toLowerCase();
+      const filtered = [...students].filter((student) => {
+        if (!query) {
+          return null;
+        } else if (
+          student.firstName.toLowerCase().includes(searchTerm) ||
+          student.lastName.toLowerCase().includes(searchTerm)
+        ) {
+          return student;
+        }
+      });
+      setFiltered(filtered);
+    };
+    filterStudentList(query);
+  }, [query]);
+
+  const listToRender = query ? filtered : students;
+
   const getAverage = (grades) => {
     let sum = grades
       .map((grade) => Number(grade))
@@ -27,18 +49,35 @@ const App = () => {
 
   return (
     <main className="container">
-      {students.map((student) => {
+      <div>
+        <input
+          className="search-input"
+          placeholder="Search by name"
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+      </div>
+      {listToRender.map((student) => {
         return (
-          <section key={student.id}>
-            <img src={student.pic} alt="profile pic" />
-            <p>
-              {student.firstName} {student.lastName}
-            </p>
-            <p>Email: {student.email}</p>
-            <p>Company: {student.company}</p>
-            <p>Skill: {student.skill}</p>
-            <p>Average: {getAverage(student.grades)}%</p>
-          </section>
+          <div key={student.id} className="student-card">
+            <img
+              src={student.pic}
+              alt="profile pic"
+              className="student-card__avatar"
+            />
+            <div className="student-card__details">
+              <h1>
+                {student.firstName} {student.lastName}
+              </h1>
+              <p>
+                Email: <a href={student.email}>{student.email}</a>
+              </p>
+              <p>Company: {student.company}</p>
+              <p>Skill: {student.skill}</p>
+              <p>Average: {getAverage(student.grades)}%</p>
+            </div>
+          </div>
         );
       })}
     </main>
